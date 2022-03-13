b0565c5cd4560ee8d695b9f337766a2a85eac09f
Gitara2014 <registrujmeodmah@gmail.com>
2022-03-14 00:45:11 +0100

    cleaning UserService.java

--- src/main/java/com/bane/asynch/service/UserService.java
+++ src/main/java/com/bane/asynch/service/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 @Log4j2
@@ -21,7 +20,7 @@ public class UserService {
         this.remoteService = remoteService;
     }
 
-    private final User dummyUser = new User(22l, "Mike Jones", 33);
+    private final User dummyUser = new User(22L, "Mike Jones", 33);
 
     public User findUser(String id) {
         try {
@@ -32,8 +31,7 @@ public class UserService {
         return dummyUser;
     }
 
-    public UserResponse gatherUserInfoSequential(String userId) throws JsonProcessingException {
-        Instant start = Instant.now();
+    public UserResponse gatherUserInfoSequential(String userId){
 
         //1. user info API call
         UserInfo user = remoteService.getUserInfo();
@@ -45,79 +43,32 @@ public class UserService {
         Address userAddress = remoteService.getUserAddress();
 
         //combine all API calls results
-        UserResponse userResponse = new UserResponse(user, userStatus, userAddress);
-
-        Instant end = Instant.now();
-        Duration duration = Duration.between(start, end);
-        log.info("All API calls, called in sequence took: {}ms", duration.toMillis());
-        return userResponse;
+        return new UserResponse(user, userStatus, userAddress);
     }
 
-    public UserResponse gatherUserInfoParallel(String userId) throws Exception {
-        log.info("gatherUserInfoParallel");
-        Instant start = Instant.now();
-
-        //1. user info API call
-        Callable<UserInfo> userInfoCallable = new Callable<UserInfo>() {
-            @Override
-            public UserInfo call() throws Exception {
-                Instant childThreadStart = Instant.now();
-                UserInfo userInfo = remoteService.getUserInfo();
-                Instant childThreadStop = Instant.now();
-                Duration duration = Duration.between(childThreadStart, childThreadStop);
-                log.info("Child Thread call took: {}ms", duration.toMillis());
-                return userInfo;
-            }
-        };
-
-        //2. user working status API call
-        Callable<WorkingStatus> userStatusCallable = remoteService::getUserStatus;
-
-        //3. user address API call
-        Callable<Address> userAddressCallable = remoteService::getUserAddress;
-
-        UserInfo userInfo = userInfoCallable.call();
-        WorkingStatus workingStatus = userStatusCallable.call();
-        Address address = userAddressCallable.call();
-
-        //combine all API calls results
-        //UserResponseCallable userResponseCallable = new UserResponseCallable(userInfoCallable, userStatusCallable, userAddressCallable);
-        UserResponse userResponse = new UserResponse(userInfo, workingStatus, address);
-
-        Instant end = Instant.now();
-        Duration duration = Duration.between(start, end);
-        log.info("All API calls, called in sequence took: {}ms", duration.toMillis());
-        return userResponse;
-    }
 
     public UserResponse gatherUserInfoThreads(String userId) throws Exception {
         log.info("gatherUserInfoParallel");
         Instant start = Instant.now();
 
         //1. user info API call
-        Callable<UserInfo> userInfoCallable = new Callable<UserInfo>() {
-            @Override
-            public UserInfo call() throws Exception {
-                Instant childThreadStart = Instant.now();
-                UserInfo userInfo = remoteService.getUserInfo();
-                Instant childThreadStop = Instant.now();
-                Duration duration = Duration.between(childThreadStart, childThreadStop);
-                log.info("Child Thread UserInfo call took: {}ms", duration.toMillis());
-                return userInfo;
-            }
+        Callable<UserInfo> userInfoCallable = () -> {
+            Instant childThreadStart = Instant.now();
+            UserInfo userInfo = remoteService.getUserInfo();
+            Instant childThreadStop = Instant.now();
+            Duration duration = Duration.between(childThreadStart, childThreadStop);
+            log.info("Child Thread UserInfo call took: {}ms", duration.toMillis());
+            return userInfo;
         };
 
         //2. user working status API call
-        Callable<WorkingStatus> userStatusCallable = new Callable<WorkingStatus>() {
-            @Override
-            public WorkingStatus call() throws Exception {
-                Instant childThreadStart = Instant.now();
-                WorkingStatus userStatus = remoteService.getUserStatus();
-                Instant childThreadStop = Instant.now();
-                Duration duration = Duration.between(childThreadStart, childThreadStop);
-                log.info("Child Thread WorkingStatus call took: {}ms", duration.toMillis());
-                return userStatus;
-            }
+        Callable<WorkingStatus> userStatusCallable = () -> {
+            Instant childThreadStart = Instant.now();
+            WorkingStatus userStatus = remoteService.getUserStatus();
+            Instant childThreadStop = Instant.now();
+            Duration duration = Duration.between(childThreadStart, childThreadStop);
+            log.info("Child Thread WorkingStatus call took: {}ms", duration.toMillis());
+            return userStatus;
         };
 
         //3. user address API call
@@ -133,7 +84,6 @@ public class UserService {
 
 
         //combine all API calls results
-        //UserResponseCallable userResponseCallable = new UserResponseCallable(userInfoCallable, userStatusCallable, userAddressCallable);
         UserResponse userResponse = new UserResponse(userInfoFutureTask.get(), workingStatusFutureTask.get(), addressFutureTask.get());
 
         Instant end = Instant.now();
